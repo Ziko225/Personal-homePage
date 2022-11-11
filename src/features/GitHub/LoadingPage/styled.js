5f9c9ab8e06773aa7e138e1e4b9195573fb9423c
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-11-11 18:20:38 +0100

    Added @media queries mobileMax to LoadingPage

--- src/features/GitHub/LoadingPage/styled.js
+++ src/features/GitHub/LoadingPage/styled.js
@@ -5,28 +5,40 @@ export const Block = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    color: ${({theme})=> theme.color.black};
+    color: ${({ theme }) => theme.color.black};
     margin-bottom: 140px;
+    @media (max-width:${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin-bottom: 60px;
+    };
 `;
 
 export const Title = styled.h3`
     font-weight: 400;
     margin: 0 0 48px;
+    font-size: 20px;
+    text-align: center;
+    @media (max-width:${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 15px;
+        margin-bottom: 24px;
+    };
 `;
 
 const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
+    from {
+        transform: rotate(0deg);
+    }
 
-  to {
-    transform: rotate(360deg);
-  }
+    to {
+        transform: rotate(360deg);
+    }
 `;
 
 export const Ico = styled(LoadingElement)`
     animation: ${rotate} 1s linear infinite;
-    color: ${({theme})=> theme.color.blue};
+    color: ${({ theme }) => theme.color.blue};
     width: 160px;
     height: auto;
+    @media (max-width:${({ theme }) => theme.breakpoint.mobileMax}px) {
+        width: 100px;
+    };
 `;
