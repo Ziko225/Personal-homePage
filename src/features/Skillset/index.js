b098193fdd7b20cd1414d4e1e9d32eaa5c0a7d4b
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-11-16 18:06:33 +0100

    Replaced Container from common to Skillset component

--- src/features/Skillset/index.js
+++ src/features/Skillset/index.js
@@ -1,42 +1,47 @@
-import Container from "../../common/Container";
-import { Li } from "./styled";
+import { Block, Content, Li, Title } from "./styled";
 
 const Skillset = () => {
     return (
         <>
-            <Container title={"My skillset includes"}>
-                <Li>JavaScript: ES6+</Li>
-                <Li>React</Li>
-                <Li>Semantic & accessible HTML</Li>
-                <Li>Styled-Components</Li>
-                <Li>CSS Grid</Li>
-                <Li>CSS Flexbox</Li>
-                <Li>React Router</Li>
-                <Li>React Hooks</Li>
-                <Li>Redux (Toolkit)</Li>
-                <Li>Working with API (fetch, axios)</Li>
-                <Li>Promises, Async/Await</Li>
-                <Li>Error handling</Li>
-                <Li>Teamwork</Li>
-                <Li>Git</Li>
-                <Li>GitHub Pull Requests & Review</Li>
-                <Li>Responsive Web Design</Li>
-                <Li>NPM</Li>
-                <Li>Immutability</Li>
-                <Li>Markdown</Li>
-                <Li>CSS BEM convention</Li>
-                <Li>Scrum</Li>
-                <Li>Trello</Li>
-                <Li>Figma (for dev)</Li>
-                <Li>Adobe Photoshop</Li>
-            </Container>
-            <Container title={"What I want to learn next"}>
-                <Li>TypeScript</Li>
-                <Li>React Context</Li>
-                <Li>Cypress</Li>
-                <Li>Node.js</Li>
-                <Li>SQL</Li>
-            </Container>
+            <Block>
+                <Title>My skillset includes</Title>
+                <Content>
+                    <Li>JavaScript: ES6+</Li>
+                    <Li>React</Li>
+                    <Li>Semantic & accessible HTML</Li>
+                    <Li>Styled-Components</Li>
+                    <Li>CSS Grid</Li>
+                    <Li>CSS Flexbox</Li>
+                    <Li>React Router</Li>
+                    <Li>React Hooks</Li>
+                    <Li>Redux (Toolkit)</Li>
+                    <Li>Working with API (fetch, axios)</Li>
+                    <Li>Promises, Async/Await</Li>
+                    <Li>Error handling</Li>
+                    <Li>Teamwork</Li>
+                    <Li>Git</Li>
+                    <Li>GitHub Pull Requests & Review</Li>
+                    <Li>Responsive Web Design</Li>
+                    <Li>NPM</Li>
+                    <Li>Immutability</Li>
+                    <Li>Markdown</Li>
+                    <Li>CSS BEM convention</Li>
+                    <Li>Scrum</Li>
+                    <Li>Trello</Li>
+                    <Li>Figma (for dev)</Li>
+                    <Li>Adobe Photoshop</Li>
+                </Content>
+            </Block>
+            <Block>
+                <Title>What I want to learn next</Title>
+                <Content>
+                    <Li>TypeScript</Li>
+                    <Li>React Context</Li>
+                    <Li>Cypress</Li>
+                    <Li>Node.js</Li>
+                    <Li>SQL</Li>
+                </Content>
+            </Block>
         </>
     )
 };
