df486b039cef7e0ccdca22a83af0c995aeec76c3
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-12-14 20:06:46 +0100

    Changed email adress

--- src/links.js
+++ src/links.js
@@ -1,5 +1,5 @@
 export const email = "nazar@bychyk.it";
 export const gitHub = "https://github.com/Ziko225";
 export const instagram = "https://www.instagram.com/nazar_bychyk";
-export const linkedin = "https://www.linkedin.com/in/nazar-bychyk-502316252";
+export const linkedin = "https://www.linkedin.com/in/nazarbychyk";
 export const facebook = "https://www.facebook.com/Ziko255";
