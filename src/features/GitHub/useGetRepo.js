770057b3b4b1c3747bf3a90ddb31cb5086d0d024
Nazar <nazar@bychyk.it>
2023-03-29 13:30:10 +0200

    Renamed result to data in useGetRepositories

--- src/features/GitHub/useGetRepo.js
+++ src/features/GitHub/useGetRepo.js
@@ -2,31 +2,31 @@ import { Octokit } from "octokit";
 import { useEffect, useState } from "react";
 
 const useGetRepositories = () => {
-    const [result, setResult] = useState("");
+    const [data, setData] = useState();
     const [status, setStatus] = useState("init");
 
     useEffect(() => {
-        const octokit = new Octokit()
-        async function getRepo() {
-            try {
-                const result = await octokit.request('GET /users/{username}/repos', {
-                    username: 'Ziko225'
-                })
-                setResult(result.data);
-                if (result) {
-                    setStatus("ok");
-                }
-            } catch {
-                setStatus("error");
-            }
+        const octokit = new Octokit();
+        try {
+            octokit.request('GET /users/{username}/repos', {
+                username: 'Ziko225'
+            }).then((value) => {
+                setData(value.data);
+            })
+        } catch {
+            setStatus("error");
         };
-        const interval = setTimeout(() => {
-            getRepo();
-            clearInterval(interval);
-        }, 1500);
+    }, []);
 
-    }, [])
-    return { result, status }
+    useEffect(() => {
+        if (data) {
+            const interval = setTimeout(() => {
+                setStatus("ok");
+                clearInterval(interval);
+            }, 1500);
+        };
+    }, [data])
+    return { data, status };
 };
 
 export default useGetRepositories;
